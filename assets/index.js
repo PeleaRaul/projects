b64f5703cb019486fc7a1f537b19002212560325
PeleaRaul <pelearauldaniel@gmail.com>
2024-01-31 09:59:13 +0200

    Initial commit

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+  // Vanilla JavaScript code to show/hide loading overlay
+  window.addEventListener('load', function() {
+    // Hide loading overlay after the entire page has loaded with smooth fade-out
+    var loadingOverlay = document.getElementById('loading-overlay');
+    loadingOverlay.style.transition = 'opacity 0.5s ease-in-out';
+    loadingOverlay.style.opacity = 0;
+
+    // Optionally, you can remove the loading overlay from the DOM after the fade-out
+    setTimeout(function() {
+      loadingOverlay.style.display = 'none';
+    }, 1500); // Adjust the duration (in milliseconds) for a smoother transition
+  });
